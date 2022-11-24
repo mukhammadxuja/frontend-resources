67ddbc6010b16e62875b20238aaf02bc5551c31a
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2022-11-24 12:36:41 +0500

    build: configuration 404 and _document file

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+const FourOvFour = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div>
+        <h1 className="text-center">404</h1>
+        <p className="text-center">Sorry this page not found!</p>
+      </div>
+    </div>
+  );
+};
+
+export default FourOvFour;
