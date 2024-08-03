78d3300bb841ec48388bc968b3da1079f1d3c61a
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2024-08-03 15:04:28 +0500

    seo

--- pages/_document.js
+++ pages/_document.js
@@ -5,30 +5,54 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="theme-color" content="#000" />
         <title>Frontend Resources</title>
         <meta
           name="description"
-          content="Frontend Resources - A curated list of the best Frontend Resources"
+          content="Frontend Resources - A curated list of the best frontend resources"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="googlebot"
+          content="index, follow, max-video-preview:-1, max-image-preview:large, max-snippet:-1"
         />
         <meta
-          name="keywords"
-          content="nextjs, SEO, meta tags, awesome website"
+          property="og:title"
+          content="Frontend Resources - A curated list of the best frontend resources"
         />
-        <meta name="author" content="Your Name" />
-        <meta property="og:title" content="My Awesome Website" />
         <meta
           property="og:description"
-          content="This is the description of my awesome website"
+          content="A curated list of the best frontend resources"
         />
-        <meta property="og:image" content="/images/tools/angular-js.png" />
-        <meta property="og:url" content="https://www.myawesomewebsite.com" />
+        <meta
+          property="og:url"
+          content="https://frontendresources.vercel.app/"
+        />
+        <meta property="og:site_name" content="Frontend Resources" />
+        <meta property="og:locale" content="en-US" />
+        <meta
+          property="og:image"
+          content="https://designresourc.es/img/og.png"
+        />
+        <meta property="og:image:width" content="1920" />
+        <meta property="og:image:height" content="1080" />
+        <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="My Awesome Website" />
+        <meta
+          name="twitter:title"
+          content="Frontend Resources - A curated list of the best frontend resources"
+        />
         <meta
           name="twitter:description"
-          content="This is the description of my awesome website"
+          content="A curated list of the best frontend resources"
         />
-        <meta name="twitter:image" content="/path/to/image.jpg" />
+        <meta
+          name="twitter:image"
+          content="https://designresourc.es/img/og.png"
+        />
+        <meta name="twitter:image:width" content="1920" />
+        <meta name="twitter:image:height" content="1080" />
+        <meta name="next-size-adjust" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
